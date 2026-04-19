2666d4c1a07d58dd95fd5978db0d9e5270e69838
Mamertas Gelžinis <uzmirsau12@gmail.com>
2026-04-19 14:17:40 +0300

    repo cleanup

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 engine = create_engine(
     DATABASE_URL,
